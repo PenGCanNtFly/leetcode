7007e2f4f5a634618e10e7baaa2a4c188668ebef
PenGCanNtFly <yxsen_linux@163.com>
2016-09-28 10:00:15 +0800

    1-Two-Sum

---
+++ 1-Two-Sum.go
@@ -0,0 +1,31 @@
+package main
+
+var (
+	//nums   = []int{2, 7, 11, 15}
+	//target = 9
+	nums   = []int{0, 1, 2, 0}
+	target = 0
+	result = make([]int, 0)
+)
+
+func twoSum(nums []int, target int) []int {
+
+	for i := 0; i < len(nums)-1; i++ {
+		for j := 1; j < len(nums); j++ {
+			if i != j && i < j {
+				if nums[i]+nums[j] == target {
+					result = append(result, i, j)
+				} else {
+					continue
+				}
+			} else {
+				continue
+			}
+		}
+	}
+	return result
+}
+
+func main() {
+	twoSum(nums, target)
+}
