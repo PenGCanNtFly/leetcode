961b85dd9674e94924228596ede6a4a520982aa2
PenGCanNtFly <yxsen_linux@163.com>
2016-09-28 10:12:39 +0800

    添加题目描述

--- 1-Two-Sum.go
+++ 1-Two-Sum.go
@@ -1,7 +1,15 @@
 /*
-* version := 1
- */
+Example:
+```
+Given nums = [2, 7, 11, 15], target = 9,
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+```
+Version = 1
+*/
 package main
+
+import "fmt"
 
 var (
 	//nums   = []int{2, 7, 11, 15}
@@ -14,10 +22,11 @@ var (
 func twoSum(nums []int, target int) []int {
 
 	for i := 0; i < len(nums)-1; i++ {
-		for j := 1; j < len(nums); j++ {
+		for j := i + 1; j < len(nums); j++ {
 			if i != j && i < j {
 				if nums[i]+nums[j] == target {
-					result = append(result, i, j)
+					result = append(result, i)
+					result = append(result, j)
 				} else {
 					continue
 				}
@@ -26,7 +35,8 @@ func twoSum(nums []int, target int) []int {
 			}
 		}
 	}
-	return result
+	fmt.Println(result)
+	return result // result: [0 3]
 }
 
 func main() {
